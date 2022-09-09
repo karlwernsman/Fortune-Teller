3cd7889f0e2d4a0335debee4f2898ebc1ea8e1af
karl wernsman <wernsmank@gmail.com>
2022-09-09 15:30:55 -0700

    Added styling with images and CSS

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-// import functions and grab DOM elements
+const askButton = document.getElementById('ask-button');
+const playAgainButton = document.getElementById('play-again');
+const inputsParent = document.getElementById('inputs-parent');
+const fortuneParent = document.getElementById('fortune-parent');
+const fortuneDisplay = document.getElementById('fortune-response');
+const fortune = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 
-// let state
+askButton.addEventListener('click', () => {
+    const randomNumber = Math.floor(Math.random() * fortune.length);
+    fortuneDisplay.textContent = fortune[randomNumber];
+    inputsParent.classList.toggle('hide');
+    fortuneParent.classList.toggle('hide');
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+playAgainButton.addEventListener('click', () => {
+    inputsParent.classList.toggle('hide');
+    fortuneParent.classList.toggle('hide');
+});
